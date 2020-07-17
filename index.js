973a20617e70075cd8fc8860c0f027ab6d604e28
Jocy Wolff <jocywolff@gmail.com>
2020-07-17 19:30:44 +0200

    (feat): multiple correct answers possible

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Question {
     constructor(data = {}) {
         this.text = data.text || '';
         this.answers = data.answers || [];
-        this.correct = data.correct || -1;
+        this.correct = data.correct || [];
     }
 }
 
@@ -18,14 +18,14 @@ function findQuestions(text) {
 
     for (rawQuestion of rawQuestions) {
         let question = new Question();
-        question.text = rawQuestion[1].replace(/\t/g, '').replace(/\n\n/g, '');
+        question.text = rawQuestion[1].replace(/\t/g, '').replace(/\n\n/g, '').replace(/^\n/g, '').replace(/\n$/g, '')
         for (let i = 2; i < 7; i++) {
-            let questionText = rawQuestion[i].replace(/[\t]/g, '')
+            let questionText = rawQuestion[i].replace(/[\t]/g, '').replace(/^\n/g, '')
             questionText.split(/\n/g).forEach((line) => {
                 if (/[xX]$/g.test(line))
-                    question.correct = i - 2
+                    question.correct.push(i - 2);
             });
-            question.answers.push(questionText.replace(/\n*[xX]*\n/, ''))
+            question.answers.push(questionText.replace(/\n+[xX]*\n*/, ''))
         }
         questions.push(question);
     }
@@ -39,7 +39,7 @@ function writeOutputFile(questions, fileName) {
         outString += question.text + '\n\n';
         for (let i = 0; i < question.answers.length; i++) {
             if (question.answers[i]) {
-                outString += '* ' + question.answers[i] + (question.correct === i ? ' x\n' : '\n')
+                outString += '* ' + question.answers[i] + (question.correct.includes(i) ? ' x\n' : '\n')
             }
         }
         outString += '\n\n'
